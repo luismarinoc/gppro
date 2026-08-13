7dcdb03b1f35eb229eedb4b38875ed9ff7f073a9
Luis Marino <66568485+luismarinoc@users.noreply.github.com>
2026-08-13 15:28:14 -0400

    feat(expense): add approverUser field to ExpenseApprovalLevelForm
    UserType::class, required: false, include_users keeps an already-
    assigned but now-disabled approver selectable (D6).

--- src/Form/ExpenseApprovalLevelForm.php
+++ src/Form/ExpenseApprovalLevelForm.php
@@ -11,6 +11,7 @@ namespace App\Form;
 
 use App\Entity\ExpenseApprovalLevel;
 use App\Form\Type\UserRoleType;
+use App\Form\Type\UserType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -20,6 +21,9 @@ final class ExpenseApprovalLevelForm extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        /** @var ExpenseApprovalLevel|null $level */
+        $level = \array_key_exists('data', $options) ? $options['data'] : null;
+
         $builder
             ->add('level', IntegerType::class, ['label' => 'expense_approval_level.level'])
             ->add('minAmount', IntegerType::class, ['label' => 'expense_approval_level.min_amount'])
@@ -29,6 +33,13 @@ final class ExpenseApprovalLevelForm extends AbstractType
                 'multiple' => false,
                 'expanded' => false,
                 'label' => 'expense_approval_level.required_role',
+            ])
+            ->add('approverUser', UserType::class, [
+                'required' => false,
+                'label' => 'expense_approval_level.approver_user',
+                'help' => 'expense_approval_level.approver_user.help',
+                // keep an already-assigned but disabled user selectable (D6)
+                'include_users' => ($level?->getApproverUser() !== null ? [$level->getApproverUser()] : []),
             ]);
     }
 
