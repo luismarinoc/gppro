c1b3a62beaff7dd866c985f260beecb47437d328
Luis Marino <66568485+luismarinoc@users.noreply.github.com>
2026-09-05 21:11:44 -0400

    chore(release): bump version to 2.62.135 [skip ci]

--- src/Constants.php
+++ src/Constants.php
@@ -17,11 +17,11 @@ final class Constants
     /**
      * The current release version
      */
-    public const VERSION = '2.62.134';
+    public const VERSION = '2.62.135';
     /**
      * The current release: major * 10000 + minor * 100 + patch
      */
-    public const VERSION_ID = 26334;
+    public const VERSION_ID = 26335;
     /**
      * The software name
      */
